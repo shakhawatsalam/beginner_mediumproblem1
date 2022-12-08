ecea3e9d71d715d3faa434192a0df078ad44a4d5
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-12-08 19:05:17 +0600

    working on side bar

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -1,7 +1,27 @@
-import React from "react";
-
+import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 function SideBar() {
-  return <div>SideBar</div>;
+    const [sidebar, setSidebar] = useState(false);
+    const showSidebar = () => setSidebar(!sidebar);
+  return (
+    <>
+      <div className='navbar'>
+        <a href='#'>
+          <FaIcons.FaBars onClick={showSidebar}/>
+        </a>
+      </div>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className='nav-menu-items'>
+          <li className='navbar-toggle'>
+            <a href='3' className='menu-bars'>
+              <AiIcons.AiOutlineClose />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default SideBar;
