e205f0b961437d0d5b3cd93648b54d88a51c72cd
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-12-08 23:32:58 +0600

    working on sidebar icons

--- src/component/SdiebarData.jsx
+++ src/component/SdiebarData.jsx
@@ -1,53 +1,57 @@
 import React from "react";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
+import * as GiIcons from "react-icons/fa";
+import * as RiIcons from "react-icons/ri";
+import * as VsIcons from "react-icons/vsc";
+import * as IoIcons from "react-icons/io";
 
 
 
 export const SideBarData = [
     {
-        title: "Home",
-        icon: <AiIcons.AiFillHome />,
+        title: "Articles",
+        // icon: <AiIcons.AiFillHome />,
         cName: 'nav-text'
     },
     {
-        title: "Home",
-        icon: <AiIcons.AiFillHome />,
+        title: "Case law",
+        // icon: <AiIcons.AiFillHome />,
         cName: 'nav-text'
     },
     {
-        title: "Home",
-        icon: <AiIcons.AiFillHome />,
+        title: "Profile",
+        // icon: <GiIcons.FaGraduationCap />,
         cName: 'nav-text'
     },
     {
-        title: "Home",
-        icon: <AiIcons.AiFillHome />,
+        title: "My profile",
+        icon: <GiIcons.FaGraduationCap />,
         cName: 'nav-text'
     },
     {
-        title: "Home",
-        icon: <AiIcons.AiFillHome />,
+        title: "My Library",
+        icon: <AiIcons.AiFillStar />,
         cName: 'nav-text'
     },
     {
-        title: "Home",
-        icon: <AiIcons.AiFillHome />,
+        title: "Alerts",
+        icon: <RiIcons.RiMessage2Fill />,
         cName: 'nav-text'
     },
     {
-        title: "Home",
-        icon: <AiIcons.AiFillHome />,
+        title: "Metrics",
+        icon: <VsIcons.VscGraph />,
         cName: 'nav-text'
     },
     {
-        title: "Home",
-        icon: <AiIcons.AiFillHome />,
+        title: "Advance Search",
+        icon: <FaIcons.FaSearch />,
         cName: 'nav-text'
     },
     {
-        title: "Home",
-        icon: <AiIcons.AiFillHome />,
+        title: "Setting",
+        icon: <IoIcons.IoMdSettings />,
         cName: 'nav-text'
     },
 ]
