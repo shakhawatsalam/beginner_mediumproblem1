e3a7982d377d947474284779190459e4714833ec
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-12-08 20:46:55 +0600

    working wiht css

---
+++ src/component/SdiebarData.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+
+
+
+export const SideBarData = [
+    {
+        title: "Home",
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: "Home",
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: "Home",
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: "Home",
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: "Home",
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: "Home",
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: "Home",
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: "Home",
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: "Home",
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+]
--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
+import { SideBarData } from "./SdiebarData.jsx";
+import './sidebar.css'
 function SideBar() {
-    const [sidebar, setSidebar] = useState(false);
-    const showSidebar = () => setSidebar(!sidebar);
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
   return (
     <>
       <div className='navbar'>
-        <a href='#'>
-          <FaIcons.FaBars onClick={showSidebar}/>
+        <a href='#' className="menu-bars">
+          <FaIcons.FaBars onClick={showSidebar} />
         </a>
       </div>
       <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
@@ -18,6 +20,16 @@ function SideBar() {
               <AiIcons.AiOutlineClose />
             </a>
           </li>
+          {SideBarData.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <a href='#'>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </>
