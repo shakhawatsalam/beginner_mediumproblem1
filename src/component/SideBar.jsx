7b7f0fd94c16fef9c13c37e184577fbac4cdfa79
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-12-09 00:01:32 +0600

    working on input

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -2,16 +2,23 @@ import React, { useState } from "react";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { SideBarData } from "./SdiebarData.jsx";
-import './sidebar.css'
+import "./sidebar.css";
 function SideBar() {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
   return (
     <>
       <div className='navbar'>
-        <a href='#' className="menu-bars">
-          <FaIcons.FaBars onClick={showSidebar} />
-        </a>
+        <div className="bar">
+          <a href='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </a>
+        </div>
+        <div className="google" >
+          <p>Google Scholar</p>
+          <input type="text" />
+          <button><FaIcons.FaSearch /></button>
+        </div>
       </div>
       <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
         <ul className='nav-menu-items' onClick={showSidebar}>
